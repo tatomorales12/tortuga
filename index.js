7d06918eb4b6bb29005afca84cc4cbe86dcdc90a
Asdrubal Morales Giraldo <tatomorales12@gmail.com>
2026-08-08 21:07:59 +0200

    Añadir interacción con el usuario

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 console.log("🐢 Tortuga - Cada Paso Cuenta");
+console.log("");
+
+rl.question("¿Cómo te llamas? ", (nombre) => {
+    console.log("");
+    console.log(`Hola, ${nombre}.`);
+    console.log("🐢 Cada paso cuenta.");
+    rl.close();
+});
